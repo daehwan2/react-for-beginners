40cea391581576bc0b5925b10987c4a8e8cd667c
daehwan2 <dhdh0482@naver.com>
2022-01-12 20:57:31 +0900

    🐛 Fix a bug

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route
+          path={`${process.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        ></Route>
+        <Route path={`${process.PUBLIC_URL}/`} element={<Home />}></Route>
       </Routes>
     </Router>
   );
